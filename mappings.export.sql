87a1409b4609602abc2c1d2f53a9793428497203
roy <roy@laptop-roy.falco-networks.com>
2013-07-22 16:00:36 +0200

    New commit - additional things

---
+++ mappings.export.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.5.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: zabbix
+-- ------------------------------------------------------
+-- Server version	5.5.32-31.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `mappings`
+--
+
+DROP TABLE IF EXISTS `mappings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mappings` (
+  `mappingid` bigint(20) unsigned NOT NULL,
+  `valuemapid` bigint(20) unsigned NOT NULL,
+  `value` varchar(64) NOT NULL DEFAULT '',
+  `newvalue` varchar(64) NOT NULL DEFAULT '',
+  PRIMARY KEY (`mappingid`),
+  KEY `mappings_1` (`valuemapid`),
+  CONSTRAINT `c_mappings_1` FOREIGN KEY (`valuemapid`) REFERENCES `valuemaps` (`valuemapid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mappings`
+--
+
+LOCK TABLES `mappings` WRITE;
+/*!40000 ALTER TABLE `mappings` DISABLE KEYS */;
+INSERT INTO `mappings` VALUES (1,1,'0','Down'),(2,1,'1','Up'),(3,2,'0','Up'),(4,2,'2','Unreachable'),(13,6,'1','Other'),(14,6,'2','OK'),(15,6,'3','Degraded'),(17,7,'1','Other'),(18,7,'2','Unknown'),(19,7,'3','OK'),(20,7,'4','NonCritical'),(21,7,'5','Critical'),(22,7,'6','NonRecoverable'),(23,5,'1','unknown'),(24,5,'2','batteryNormal'),(25,5,'3','batteryLow'),(26,4,'1','unknown'),(27,4,'2','notInstalled'),(28,4,'3','ok'),(29,4,'4','failed'),(30,4,'5','highTemperature'),(31,4,'6','replaceImmediately'),(32,4,'7','lowCapacity'),(33,3,'0','Running'),(34,3,'1','Paused'),(35,3,'3','Pause pending'),(36,3,'4','Continue pending'),(37,3,'5','Stop pending'),(38,3,'6','Stopped'),(39,3,'7','Unknown'),(40,3,'255','No such service'),(41,3,'2','Start pending'),(49,9,'1','unknown'),(50,9,'2','running'),(51,9,'3','warning'),(52,9,'4','testing'),(53,9,'5','down'),(61,8,'1','up'),(62,8,'2','down'),(63,8,'3','testing'),(64,8,'4','unknown'),(65,8,'5','dormant'),(66,8,'6','notPresent'),(67,8,'7','lowerLayerDown'),(68,10,'1','Up'),(69,11,'1','up'),(70,11,'2','down'),(71,11,'3','testing'),(72,12,'1','idle'),(73,12,'2','connect'),(74,12,'3','active'),(75,12,'4','opensent'),(76,12,'5','openconfirm'),(77,12,'6','established'),(78,13,'1','stop'),(79,13,'2','start'),(80,14,'1','down'),(81,14,'2','attempt'),(82,14,'3','init'),(83,14,'4','twoWay'),(84,14,'5','exchangeStart'),(85,14,'6','exchange'),(86,14,'7','loading'),(87,14,'8','full'),(88,15,'1','enabled'),(89,15,'2','disabled'),(90,16,'1','initialize'),(91,16,'2','backup'),(92,16,'3','master'),(93,17,'1','up'),(94,17,'2','down');
+/*!40000 ALTER TABLE `mappings` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-07-22 15:59:35
